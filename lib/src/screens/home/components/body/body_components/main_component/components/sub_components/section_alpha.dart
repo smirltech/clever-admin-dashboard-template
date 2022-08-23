6a109154188928daf67b9aad446ddd9b64e49933
Francis Nduba Numbi <francisnnumbi@yandex.com>
2022-08-23 20:37:12 +0200

    update

--- lib/src/screens/home/components/body/body_components/main_component/components/sub_components/section_alpha.dart
+++ lib/src/screens/home/components/body/body_components/main_component/components/sub_components/section_alpha.dart
@@ -41,9 +41,98 @@ class SectionAlpha extends StatelessWidget {
                     const SizedBox(
                       height: 12,
                     ),
-                    LineChart(
-                      LineChartData(
-                        gridData: FlGridData(),
+                    Expanded(
+                      child: LineChart(
+                        LineChartData(
+                          minX: 0,
+                          maxX: 10,
+                          minY: 0,
+                          maxY: 150,
+                          titlesData: FlTitlesData(
+                            topTitles: AxisTitles(
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
+                            rightTitles: AxisTitles(
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
+                            bottomTitles: AxisTitles(
+                              sideTitles: SideTitles(
+                                  showTitles: true,
+                                 interval: 1,
+                                  getTitlesWidget: (value, _) {
+                                    switch (value.toInt()) {
+                                      case 0:
+                                        return const Text("Jan");
+                                      case 1:
+                                        return const Text("Feb");
+                                      case 2:
+                                        return const Text("Mar");
+                                      case 3:
+                                        return const Text("Apr");
+                                      case 4:
+                                        return const Text("May");
+                                      case 5:
+                                        return const Text("Jun");
+                                      case 6:
+                                        return const Text("Jul");
+                                      case 7:
+                                        return const Text("Aug");
+                                      case 8:
+                                        return const Text("Sep");
+                                      case 9:
+                                        return const Text("Oct");
+                                      case 10:
+                                        return const Text("Nov");
+                                      default:
+                                        return const Text("");
+                                    }
+                                    return SizedBox();
+                                  }),
+                            ),
+                          ),
+                          gridData: FlGridData(show: false),
+                          borderData: FlBorderData(show: false),
+                          lineBarsData: [
+                            LineChartBarData(
+                              isCurved: true,
+                              color: purple,
+                              barWidth: 3,
+                              dotData: FlDotData(show: false),
+                              spots: const [
+                                FlSpot(0, 29),
+                                FlSpot(1, 55),
+                                FlSpot(2, 70),
+                                FlSpot(3, 89),
+                                FlSpot(4, 80),
+                                FlSpot(5, 65),
+                                FlSpot(6, 90),
+                                FlSpot(7, 120),
+                                FlSpot(8, 95),
+                                FlSpot(9, 110),
+                                FlSpot(10, 130),
+                              ],
+                            ),
+                            LineChartBarData(
+                              color: red,
+                              isCurved: true,
+                              barWidth: 3,
+                              dotData: FlDotData(show: false),
+                              spots: const [
+                                FlSpot(0, 50),
+                                FlSpot(1, 70),
+                                FlSpot(2, 40),
+                                FlSpot(3, 30),
+                                FlSpot(4, 15),
+                                FlSpot(5, 15),
+                                FlSpot(6, 35),
+                                FlSpot(7, 90),
+                                FlSpot(8, 70),
+                                FlSpot(9, 80),
+                                FlSpot(10, 100),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
